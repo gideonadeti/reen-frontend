6baceef9391fb4f6809b87b61ea4d893b47282fa
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-11 19:42:42 +0000

    feat(header): Add Header component with theme and user controls

---
+++ src/app/(main)/components/header.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import { useTheme } from "next-themes";
+import { ShoppingCart } from "lucide-react";
+
+import ThemeChanger from "./theme-changer";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import { H5 } from "@/components/ui/typography";
+import { Button } from "@/components/ui/button";
+
+const Header = () => {
+  const { theme, systemTheme } = useTheme();
+
+  return (
+    <header className="flex items-center ps-2 pe-4 py-2 border-b sticky top-0 bg-background z-50">
+      <SidebarTrigger />
+      <Separator orientation="vertical" className="mx-2 !h-8" />
+      <Link href="/products">
+        <H5>REEN</H5>
+      </Link>
+      <div className="ms-auto flex items-center">
+        <UserButton
+          appearance={{
+            elements: {
+              userButtonOuterIdentifier: `${
+                theme === "light" || systemTheme === "light"
+                  ? ""
+                  : "!text-white"
+              }`,
+            },
+          }}
+          showName
+        />
+        <Separator orientation="vertical" className="mx-2 !h-8" />
+        <ThemeChanger />
+        <Separator orientation="vertical" className="mx-2 !h-8" />
+        <Button variant="outline" size="icon" className="w-8 h-8 rounded-full">
+          <ShoppingCart />
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AppSidebar from "./components/app-sidebar";
+import Header from "./components/header";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -8,6 +9,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <SidebarProvider>
       <AppSidebar />
       <div className="flex-1 flex flex-col">
+        <Header />
         <main className="flex-1">{children}</main>
       </div>
     </SidebarProvider>
