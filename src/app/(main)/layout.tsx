27739adc0ea964d9b7ec8d404f28c81fa81baf48
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-11 19:59:21 +0000

    feat(query): Add React Query and Devtools for data fetching
    Integrate @tanstack/react-query and @tanstack/react-query-devtools
    to manage server state and enhance data fetching capabilities.
    Wrap the application in a new QCProvider component for context.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,17 +2,20 @@
 
 import AppSidebar from "./components/app-sidebar";
 import Header from "./components/header";
+import QCProvider from "./components/q-c-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <div className="flex-1 flex flex-col">
-        <Header />
-        <main className="flex-1">{children}</main>
-      </div>
-    </SidebarProvider>
+    <QCProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <main className="flex-1">{children}</main>
+        </div>
+      </SidebarProvider>
+    </QCProvider>
   );
 };
 
