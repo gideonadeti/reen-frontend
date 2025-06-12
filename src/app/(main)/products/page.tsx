5b96d628fd22003c1b94ab0050a7fc847834a9ce
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-12 20:43:26 +0000

    feat(products): Implement Products page with loading state
    Add functionality to display products using ProductsTable component.
    Handle loading state while fetching products data.

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import useProducts from "./hooks/use-products";
+import columns from "./components/data-table/columns";
+import ProductsTable from "./components/data-table/products-table";
+import Loading from "@/app/loading";
+import { H3 } from "@/components/ui/typography";
+
 const Page = () => {
-  return null;
+  const { productsQuery } = useProducts();
+  const products = productsQuery.data || [];
+
+  if (productsQuery.isPending) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="px-8 space-y-4">
+      <H3>Products</H3>
+      <div>
+        <ProductsTable columns={columns} data={products} />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
