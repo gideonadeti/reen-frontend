65a74cf9f66e65de245a0d15568cea10b86d43e4
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-12 20:40:38 +0000

    feat(money): Add formatMoney utility for currency formatting

---
+++ src/app/(main)/utils/format-money.ts
@@ -0,0 +1,11 @@
+const formatMoney = (amount: number) => {
+  return (
+    "$" +
+    amount.toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })
+  );
+};
+
+export default formatMoney;
