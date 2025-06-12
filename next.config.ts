8a1caf2710b44032de165d645dc608ac1f56103b
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-12 20:42:46 +0000

    feat(next.config): Configure remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import { URL } from "url";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://robohash.org/**")],
+  },
 };
 
 export default nextConfig;
