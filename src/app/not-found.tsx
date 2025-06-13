bb4a887f2fcb42aa7ad2dfab30aa61466c032cd2
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-13 12:01:44 +0000

    refactor(not-found): Add functionality to return to previous page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,32 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
 import { H1 } from "@/components/ui/typography";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh]">
       <H1>404</H1>
       <p className="text-muted-foreground mb-6">Page not found</p>
-      <Button asChild variant="outline" size="sm">
-        <Link href="/products">Return home</Link>
-      </Button>
+      <div className="flex gap-2">
+        <Button asChild variant="outline" size="sm">
+          <Link href="/products">Return home</Link>
+        </Button>
+        <Button
+          asChild
+          variant="outline"
+          size="sm"
+          onClick={() => router.back()}
+          className="cursor-pointer"
+        >
+          <span>Return back</span>
+        </Button>
+      </div>
     </div>
   );
 }
