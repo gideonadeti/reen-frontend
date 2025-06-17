00d897c4c4e5c5f59735e9b4da0fb87c3943461c
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-17 19:55:09 +0000

    feat(orders): Add orders data table and row actions

---
+++ src/app/(main)/orders/components/data-table/columns.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import Header from "@/app/(main)/components/data-table/header";
+import formatMoney from "@/app/(main)/utils/format-money";
+import RowActions from "./row-actions";
+import { Order } from "../../types/order";
+import formatDate from "../../utils/format-date";
+
+const columns: ColumnDef<Order>[] = [
+  {
+    accessorKey: "total",
+    header: ({ column }) => <Header column={column} title="Total Cost" />,
+    cell: ({ row }) => {
+      const order = row.original as Order;
+
+      return <span className="ps-4">{formatMoney(order.total)}</span>;
+    },
+  },
+  {
+    accessorKey: "orderItems",
+    header: ({ column }) => (
+      <Header column={column} title="Number of Order Items" />
+    ),
+    cell: ({ row }) => {
+      const order = row.original as Order;
+
+      return <span className="ps-4">{order.orderItems.length}</span>;
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => <Header column={column} title="Created At" />,
+    cell: ({ row }) => {
+      const order = row.original as Order;
+
+      return <span className="ps-4">{formatDate(order.createdAt)}</span>;
+    },
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    cell: ({ row }) => <RowActions row={row} />,
+  },
+];
+
+export default columns;
