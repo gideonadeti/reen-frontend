8f5e511e179324d95f398e99366a5891e274c53d
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-24 00:36:52 +0000

    feat(user): Add additional user properties and reorder fields

--- src/app/(main)/types/user.ts
+++ src/app/(main)/types/user.ts
@@ -1,8 +1,23 @@
 export interface User {
   id: string;
   name: string;
-  role: UserRole;
   balance: number;
+  salesCount: number;
+  purchasesCount: number;
+  amountGained: number;
+  amountSpent: number;
+  createdAt: Date;
+  updatedAt: Date;
+
+  role: UserRole;
+  balances: Balance[];
 }
 
 export type UserRole = "ADMIN" | "NADMIN";
+
+export interface Balance {
+  id: string;
+  amount: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
