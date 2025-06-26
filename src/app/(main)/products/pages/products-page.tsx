5a844465ea52e918a72ef4e8f571427e50e46b11
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-26 05:04:25 +0000

    fix(products): Refactor product filtering and user handling

--- src/app/(main)/products/pages/products-page.tsx
+++ src/app/(main)/products/pages/products-page.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from "next/navigation";
 
 import useProducts from "../hooks/use-products";
 import useUser from "../../hooks/use-user";
+import useUsers from "../../users/hooks/use-users";
 import Loading from "@/app/loading";
 import ProductsTable from "../components/data-table/products-table";
 import columns from "../components/data-table/columns";
@@ -12,22 +13,23 @@ import { H3 } from "@/components/ui/typography";
 const ProductsPage = () => {
   const { productsQuery } = useProducts();
   const { userQuery } = useUser();
+  const { usersQuery } = useUsers();
+  const users = usersQuery.data || [];
   let products = productsQuery.data || [];
-  const user = userQuery.data;
+  const authUser = userQuery.data;
   const searchParams = useSearchParams();
-  const mine = searchParams.get("mine");
+  const adminId = searchParams.get("adminId");
+  const user = users.find((user) => user.id === adminId);
+  const isAuthUser = authUser?.id === adminId;
+  products = products.filter((product) => product.adminId === adminId);
 
-  if (mine === "true") {
-    products = products.filter((product) => product.adminId === user?.id);
-  }
-
-  if (productsQuery.isPending) {
+  if (productsQuery.isPending || userQuery.isPending || usersQuery.isPending) {
     return <Loading />;
   }
 
   return (
     <div className="p-4 pt-0 space-y-4">
-      <H3>{mine === "true" ? "My " : ""}Products</H3>
+      <H3>{isAuthUser ? "My " : `${user?.name}'s `}Products</H3>
       <div>
         <ProductsTable columns={columns} data={products} />
       </div>
