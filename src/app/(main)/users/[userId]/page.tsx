9eb2329fd42a3440fca8c034e9be63a55ec664b1
Gideon Adeti <gideonadeti0@gmail.com>
2025-07-05 10:53:14 +0000

    refactor(page): Redirect anonymous users to users list

--- src/app/(main)/users/[userId]/page.tsx
+++ src/app/(main)/users/[userId]/page.tsx
@@ -56,6 +56,12 @@ const Page = () => {
     );
   }
 
+  if (user.role === "ANONYMOUS") {
+    router.replace("/users");
+
+    return null;
+  }
+
   return (
     <div className="px-4 pb-4 space-y-4">
       <section>
