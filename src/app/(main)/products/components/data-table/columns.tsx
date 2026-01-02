714c904031aa038132bce9dd8a6bd44a49df1213
Gideon Adeti <gideonadeti0@gmail.com>
2026-01-02 13:22:39 +0000

    fix(columns): Correct header title for admin name in data table

--- src/app/(main)/products/components/data-table/columns.tsx
+++ src/app/(main)/products/components/data-table/columns.tsx
@@ -53,7 +53,7 @@ const columns: ColumnDef<Product>[] = [
   },
   {
     accessorKey: "admin.name",
-    header: ({ column }) => <Header column={column} title="Admin's Name" />,
+    header: ({ column }) => <Header column={column} title="Admin Name" />,
     cell: ({ row }) => {
       const product = row.original as Product;
 
